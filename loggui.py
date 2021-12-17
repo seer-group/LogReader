309435bf452e52b6f4f3abd15c4b8456da3e5f31
qiangsheng <qiangsheng.huang@foxmail.com>
2021-12-17 12:26:04 +0800

    2.3.1.a

--- loggui.py
+++ loggui.py
@@ -983,7 +983,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.close()
 
     def about(self):
-        QtWidgets.QMessageBox.about(self, "关于", """Log Viewer V2.3.0.a""")
+        QtWidgets.QMessageBox.about(self, "关于", """Log Viewer V2.3.1.a""")
 
     def ycombo_onActivated(self):
         curcombo = self.sender()
