0430e16081ed6f19c42ad1207132bb83fcac6bf6
chending <chending_6202320@126.com>
2022-04-26 17:57:17 +0800

    饼图库改用Qchart

--- CPUPieView.py
+++ CPUPieView.py
@@ -1,24 +1,24 @@
-import sys
+import datetime
 import re
-import matplotlib
-matplotlib.use("Qt5Agg")
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QSlider, QLabel, QCheckBox
+from PyQt5.QtChart import QPieSeries, QPieSlice, QChartView, QChart
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QSlider, QLabel, QCheckBox, QSpacerItem, QSizePolicy
 from PyQt5.QtCore import Qt, pyqtSignal, QThread
-from PyQt5.QtGui import QCloseEvent
+from PyQt5.QtGui import QCloseEvent, QBrush, QColor, QFont
+
 
 class LoadDataTread(QThread):
     readReady = pyqtSignal()
+
     def __init__(self, readThred, parent=None):
         super(LoadDataTread, self).__init__(parent)
         self.readThread = readThred
-        self.outerData = []
-        self.innerData = []
+        self.generalData = []
+        self.detailedData = []
         self.oriData = []
+
     def run(self) -> None:
-        self.outerData = []
-        self.innerData = []
+        self.generalData = []
+        self.detailedData = []
         self.oriData = []
         if not self.readThread:
             return
@@ -27,12 +27,12 @@ class LoadDataTread(QThread):
                 continue
             self.oriData.append(line)
             temp = re.split(r"\[|\]", line)
-            timeStr = temp[1]
+            dateTime = datetime.datetime.strptime(temp[1], '%y%m%d %H%M%S.%f')
             temp = temp[-2].split("|")
             name = ["System", "free", "rbkProc"]
             num = ["", "", ""]
             ratio = [float(temp[0]), 100 - float(temp[0]) - float(temp[1]), float(temp[1])]
-            self.outerData.append([timeStr, name, num, ratio])
+            self.generalData.append([dateTime, name, num, ratio])
             name = ["System", "free"]
             num = ["", ""]
             ratio = [float(temp[0]), 100 - float(temp[0]) - float(temp[1])]
@@ -45,95 +45,138 @@ class LoadDataTread(QThread):
                 index += 3
             name.append("other")
             num.append("")
-            ratio.append(self.outerData[-1][-1][-1] - sum(ratio[2:]))
-            self.innerData.append([timeStr, name, num, ratio])
+            ratio.append(self.generalData[-1][-1][-1] - sum(ratio[2:]))
+            self.detailedData.append([dateTime, name, num, ratio])
         self.readReady.emit()
+
+
+class MyChart(QChart):
+    def __init__(self):
+        super(MyChart, self).__init__()
+        self.legend().setAlignment(Qt.AlignRight)
+        self.legend().setBackgroundVisible(True)
+        self.legend().setBrush(QBrush(QColor(128, 128, 128, 128)))
+        self.innerSliceLabelVisible = True
+        self.outerSliceLabelVisible = True
+        self.innerSliceLabelPosition = QPieSlice.LabelInsideNormal
+        self.outerSliceLabelPosition = QPieSlice.LabelOutside
+
+    def _slotPieSeriesHovered(self, slice: QPieSlice, state: bool):
+        currentSeries: QPieSeries = slice.parent()
+        if state:
+            [s.setVisible(False) for s in self.series() if s != currentSeries]
+            [s.setLabelVisible(False) for s in currentSeries.slices() if s != slice]
+            slice.setExplodeDistanceFactor(0.03)
+            slice.setExploded(state)
+        else:
+            [s.setVisible(True) for s in self.series()]
+            if (currentSeries.holeSize() and self.outerSliceLabelVisible) or \
+                    (not currentSeries.holeSize() and self.innerSliceLabelVisible):
+                [s.setLabelVisible(True) for s in currentSeries.slices()]
+            slice.setExploded(state)
+
+    def updateSeries(self, d1, d2):
+        self.removeAllSeries()
+        pieSeries1 = QPieSeries()
+        pieSeries2 = QPieSeries()
+        pieSeries1.setPieSize(0.75)
+        pieSeries1.setHoleSize(0.6)
+        pieSeries2.setPieSize(0.6)
+        for l, v in d1:
+            temp = QPieSlice("%s:%.2f%%" % (l, v), v)
+            temp.setBorderColor(Qt.gray)
+            temp.setBorderWidth(2)
+            if self.outerSliceLabelVisible and v:
+                temp.setLabelVisible()
+                temp.setLabelPosition(self.outerSliceLabelPosition)
+            pieSeries1.append(temp)
+        for l, v in d2:
+            temp = QPieSlice("%s:%.2f%%" % (l, v), v)
+            temp.setBorderColor(Qt.gray)
+            temp.setBorderWidth(2)
+            if self.innerSliceLabelVisible and v:
+                temp.setLabelVisible()
+                temp.setLabelPosition(self.innerSliceLabelPosition)
+                temp.setLabelColor(Qt.white)
+            pieSeries2.append(temp)
+        self.addSeries(pieSeries1)
+        self.addSeries(pieSeries2)
+
+        pieSeries1.hovered.connect(self._slotPieSeriesHovered)
+        pieSeries2.hovered.connect(self._slotPieSeriesHovered)
+
 
 class CPUPieView(QWidget):
     closed = pyqtSignal()
-    def __init__(self,readThread, parent=None):
+
+    def __init__(self, readThread, parent=None):
         super(CPUPieView, self).__init__(parent)
         self.readThread = readThread
         self.setWindowTitle("CPU pie view")
         self.setLayout(QVBoxLayout())
-        self.canvas = FigureCanvasQTAgg(Figure())
-        self.ax1 = self.canvas.figure.add_subplot(111)
-        self.outerHandles = None
-        self.innerHandles = None
-        self.outerLegend = None
-        self.innerLegend = None
-        self.ax1.axis("off")
+        self.chart = MyChart()
+        self.chartView = QChartView(self)
+        self.chartView.setChart(self.chart)
         self.slider = QSlider(Qt.Horizontal, self)
         self.label = QLabel()
         self.label.setFixedHeight(15)
-        self.legendCheckBox = QCheckBox("Show Legend")
+        self.legendCheckBox = QCheckBox("Legend")
         self.legendCheckBox.setChecked(True)
+        self.innerLabelCheckBox = QCheckBox("Inner Slice label")
+        self.innerLabelCheckBox.setChecked(True)
+        self.outerLabelCheckBox = QCheckBox("Outer Slice label")
+        self.outerLabelCheckBox.setChecked(True)
         hLayout = QHBoxLayout(self)
         hLayout.addWidget(self.label)
+        hLayout.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
         hLayout.addWidget(self.legendCheckBox)
-        self.layout().addWidget(self.canvas)
+        hLayout.addWidget(self.innerLabelCheckBox)
+        hLayout.addWidget(self.outerLabelCheckBox)
+        self.layout().addWidget(self.chartView)
         self.layout().addLayout(hLayout)
         self.layout().addWidget(self.slider)
         self.load = None
         self.slider.setDisabled(True)
 
-        self.slider.valueChanged.connect(self._slotSliderValueChanged)
-        self.legendCheckBox.clicked.connect(self._slotLegendCheckBoxClicked)
+        self.slider.valueChanged.connect(self.updateChart)
+        self.legendCheckBox.clicked.connect(self.chart.legend().setVisible)
+        self.innerLabelCheckBox.clicked.connect(self._slotInnerLabelCheckBoxClicked)
+        self.outerLabelCheckBox.clicked.connect(self._slotOuterLabelCheckBoxClicked)
 
     def closeEvent(self, a0: QCloseEvent) -> None:
         self.closed.emit()
 
-    def _slotLegendCheckBoxClicked(self,v):
-        if v:
-            self._showLegend(self.slider.value())
-        else:
-            try:
-                self.outerLegend.remove()
-                self.innerLegend.remove()
-            except:
-                pass
-        self.canvas.draw()
-    def _slotSliderValueChanged(self,i):
-        self.updateCanvas(i)
+    def _slotInnerLabelCheckBoxClicked(self, v):
+        self.chart.innerSliceLabelVisible = v
+        self.updateChart(self.slider.value())
+
+    def _slotOuterLabelCheckBoxClicked(self, v):
+        self.chart.outerSliceLabelVisible = v
+        self.updateChart(self.slider.value())
 
     def loadData(self):
         def _slotReadReady():
-            self.slider.setMinimum(0)
-            self.slider.setMaximum(len(self.load.outerData) - 1)
-            self.slider.setDisabled(False)
-            self.updateCanvas(0)
+            if self.load.oriData:
+                self.slider.setMinimum(0)
+                self.slider.setMaximum(len(self.load.generalData) - 1)
+                self.slider.setDisabled(False)
+                self.slider.setValue(0)
+                self.updateChart(0)
+            else:
+                self.removeSeries()
+                self.slider.setDisabled(True)
+
         self.load = LoadDataTread(self.readThread)
         self.load.readReady.connect(_slotReadReady)
         self.load.start()
 
-    def updateCanvas(self, index):
-        self.ax1.cla()
-        try:
-            self.outerLegend.remove()
-            self.innerLegend.remove()
-        except:
-            pass
-        self.label.setText(f"time:{self.load.outerData[index][0]}\t{index+1}/{len(self.load.outerData)}")
+    def removeSeries(self):
+        self.chart.removeAllSeries()
+
+    def updateChart(self, index):
+        d1 = zip(self.load.detailedData[index][1], self.load.detailedData[index][-1])
+        d2 = zip(self.load.generalData[index][1], self.load.generalData[index][-1])
+        self.chart.updateSeries(d1, d2)
+
+        self.label.setText(f"time: {self.load.generalData[index][0]}\t{index + 1}/{len(self.load.generalData)}")
         self.label.setToolTip(self.load.oriData[index])
-        self.outerHandles, _, _ = self.ax1.pie(self.load.outerData[index][-1], radius=1.3, autopct='%1.2f%%', pctdistance=1.2, wedgeprops=dict(width=0.3, edgecolor="#FFFFFF"), startangle=90)
-        self.innerHandles, _, _ = self.ax1.pie(self.load.innerData[index][-1], radius=1, autopct='%1.2f%%', pctdistance=0.9, wedgeprops=dict(edgecolor="#FFFFFF"), startangle=90)
-        if self.legendCheckBox.isChecked():
-            self._showLegend(index)
-        self.canvas.draw()
-        self.canvas.flush_events()
-        # ax.axis('equal')
-
-    def _showLegend(self, index):
-        outerLabels = [f"{self.load.outerData[index][1][i]}: {self.load.outerData[index][-1][i]}" for i in range(len(self.load.outerData[index][1]))]
-        innerLabels = [f"{self.load.innerData[index][1][i]}: {self.load.innerData[index][-1][i]}" for i in range(len(self.load.innerData[index][1]))]
-        self.outerLegend = self.canvas.figure.legend(handles=self.outerHandles, title="Outer", labels=outerLabels, loc="upper left")
-        n = round(len(self.load.innerData[index][1])/40)+1
-        self.innerLegend = self.canvas.figure.legend(handles=self.innerHandles, title="Inner", labels=innerLabels, ncol=n, loc="center right")
-        self.outerLegend.set_draggable(state=True)
-        self.innerLegend.set_draggable(state=True)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    c =CPUPieView(None)
-    c.show()
-    app.exec()
