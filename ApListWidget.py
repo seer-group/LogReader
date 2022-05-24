f85338366b8ad1df5c739028054185f5366259f5
chending <chending_6202320@126.com>
2022-05-24 11:41:41 +0800

    ApListWidget增加reset按钮

--- ApListWidget.py
+++ ApListWidget.py
@@ -1,10 +1,10 @@
 import re
 from datetime import datetime
 
-from PyQt5.QtCore import Qt, pyqtSignal, QThread, QDateTime, QPoint, QRectF, QPointF
+from PyQt5.QtCore import Qt, pyqtSignal, QThread, QDateTime, QPoint, QPointF, QRectF
 from PyQt5.QtGui import QPainter, QColor, QCursor
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
-from PyQt5.QtChart import QChartView, QChart, QScatterSeries, QDateTimeAxis
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QSpacerItem, QSizePolicy
+from PyQt5.QtChart import QChartView, QChart, QScatterSeries, QDateTimeAxis, QValueAxis
 
 
 class LoadDataTread(QThread):
@@ -40,32 +40,32 @@ class LoadDataTread(QThread):
 class MyChart(QChart):
     def __init__(self):
         super(MyChart, self).__init__()
-        # 动画在拖动时会卡顿
+        # 动画在拖动时看上去卡顿
         # self.setAnimationOptions(QChart.SeriesAnimations)
         self.valueLabel = QLabel()
         self.valueLabel.setWindowFlags(Qt.ToolTip)
-        self.leftButtonPressed = False
-        self.pressPos = None
+        self.isLBPressed = False
+        self.oPos = None
 
     def mousePressEvent(self, event) -> None:
         if event.button() == Qt.LeftButton:
-            self.leftButtonPressed = True
-            self.pressPos = event.pos()
+            self.isLBPressed = True
+            self.oPos = event.pos()
 
     def mouseMoveEvent(self, event) -> None:
-        if self.leftButtonPressed:
-            deltaPos = event.pos() - self.pressPos
+        if self.isLBPressed:
+            deltaPos = event.pos() - self.oPos
             self.scroll(-deltaPos.x(), deltaPos.y())
-            self.pressPos = event.pos()
+            self.oPos = event.pos()
 
     def mouseReleaseEvent(self, event) -> None:
         if event.button() == Qt.LeftButton:
-            self.leftButtonPressed = False
+            self.isLBPressed = False
 
     def wheelEvent(self, event) -> None:
-        v = 1.2 if event.delta() > 0 else 0.8
-        area:QRectF = self.plotArea()
-        centerPoint:QPointF = area.center()
+        v = 0.8 if event.delta() > 0 else 1.2
+        area = self.plotArea()
+        centerPoint = area.center()
         area.setWidth(area.width()*v)
         area.setHeight(area.height()*v)
         newCenterPoint = QPointF(2*centerPoint-event.pos()-(centerPoint-event.pos())/v)
@@ -77,7 +77,7 @@ class MyChart(QChart):
         if len(data) < 1:
             return
         dateAxisX = QDateTimeAxis(self)
-        dateAxisX.setFormat("hh:mm:ss")
+        dateAxisX.setFormat("dd hh:mm:ss")
         for k, v in data.items():
             scatterSeries = QScatterSeries(self)
             scatterSeries.setMarkerSize(8)
@@ -113,9 +113,16 @@ class ApListWidget(QWidget):
         self.setWindowTitle("AP信号强度")
         self.chart = MyChart()
         self.chartView = QChartView(self.chart)
+        self.resetButton = QPushButton("Reset")
         self.chartView.setRenderHint(QPainter.Antialiasing)
+        self.hBoxLayout = QHBoxLayout()
+        self.hBoxLayout.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        self.hBoxLayout.addWidget(self.resetButton)
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.chartView)
+        self.layout().addLayout(self.hBoxLayout)
+
+        self.resetButton.clicked.connect(self.resetButtonClicked)
 
     def closeEvent(self, event) -> None:
         self.closed.emit()
@@ -124,3 +131,8 @@ class ApListWidget(QWidget):
         self.load = LoadDataTread(self.readThread)
         self.load.finished.connect(lambda: self.chart.updateSeries(self.load.data))
         self.load.start()
+
+    def resetButtonClicked(self):
+        # 通过记录坐标轴数据恢复有时会有Bug数据正确但是显示不正确,改为重绘
+        if self.load and self.load.isFinished():
+            self.chart.updateSeries(self.load.data)
